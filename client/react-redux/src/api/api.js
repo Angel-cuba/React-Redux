03a4088934a3e35803032d359ed1f295c9bc90f2
Angel <araozangel842@gmail.com>
2021-09-26 20:43:27 +0300

    Funciones en archivo separado, usando Axios

--- client/react-redux/src/api/api.js
+++ client/react-redux/src/api/api.js
@@ -3,3 +3,5 @@ import axios from 'axios';
 const url = 'http://localhost:3002/api'
 
 export const fetchPost = () => axios.get(`${url}/read`)
+
+export const fetchCreate = (newPost) => axios.post(`${url}/create`,  newPost )
