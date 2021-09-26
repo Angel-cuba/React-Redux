3df20aac178f88db5246008599d22c45237825e8
Angel <araozangel842@gmail.com>
2021-09-26 22:37:00 +0300

    Mapping all posts in one post component

--- client/react-redux/src/components/Posts/Posts.js
+++ client/react-redux/src/components/Posts/Posts.js
@@ -2,16 +2,24 @@ import React from 'react'
 import Post from './Post/Post'
 import useStyles from './styles'
 
-const Posts = () => {
-     const classes = useStyles()
-     return (
-          <>
-           <h1>Post</h1>
-           <Post/>
-           <Post/>
-           <Post/>
+import { Grid, CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux'
 
-          </>
+const Posts = () => {
+     const posts = useSelector((state) => state.posts)
+     const classes = useStyles()
+
+     console.log(posts);
+     return (
+         !posts.length ? <CircularProgress/> : (
+              <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
+                    {posts.map((post) => (
+                         <Grid key={post._id} item xs={12} sm={6}>
+                                   <Post post={post}/>
+                         </Grid>
+                    ))}
+              </Grid>
+         )
          
      )
 }
