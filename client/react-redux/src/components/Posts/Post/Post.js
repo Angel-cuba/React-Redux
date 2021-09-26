79c136a61428c91cc9e4781bd4248d4733402eda
Angel <araozangel842@gmail.com>
2021-09-26 22:39:51 +0300

    View of post component

--- client/react-redux/src/components/Posts/Post/Post.js
+++ client/react-redux/src/components/Posts/Post/Post.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import useStyles from './styles'
+import { Card, CardActions, CardContent, CarMedia, Button, Typography, CardMedia } from '@material-ui/core';
 
-const Post = () => {
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteIcon from '@mui/icons-material/Delete';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+import moment from 'moment';
+
+const Post = ({ post }) => {
+     console.log(post);
      const classes = useStyles()
      return (
-          <h1>Post ----1</h1>
+          <Card className={classes.card} >
+           <CardMedia className={classes.media} title={post.title}/>
+           <div className={classes.overlay}>
+                <Typography variant="h6">{post.creator}</Typography>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+           </div>
+               <div className={classes.overlay2}>
+                    <Button styles={{ color: 'white' }} size="small" onClick={() => {}}>
+                         <MoreHorizIcon fontSize="default" />
+                    </Button>
+               </div>
+               <div className={`${classes.details} ${classes.title}`}>
+                     <Typography variant="body2">{post.message}</Typography>
+                      <Typography className={classes.title1} variant="body2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+               </div>
+          </Card>
      )
 }
 
