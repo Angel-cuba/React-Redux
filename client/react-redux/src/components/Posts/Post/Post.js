259665c54fe5d8b941ce708d57488ce3830c5e38
Angel <araozangel842@gmail.com>
2021-09-26 09:36:56 +0300

    Working

---
+++ client/react-redux/src/components/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import useStyles from './styles'
+
+const Post = () => {
+     const classes = useStyles()
+     return (
+          <h1>Post ----1</h1>
+     )
+}
+
+export default Post;
--- client/react-redux/src/index.js
+++ client/react-redux/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import reducers from './reducers/index'
+
+
+const store = createStore(reducers,compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<Provider store={store}>
+    <App /> 
+</Provider>
+ 
+,  document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//<React.StrictMode> </React.StrictMode>
+// Note that StrictMode is a tool for highlighting potential problems in a React application. It activates additional checks and warnings such as:
+
+// Identifying components with unsafe lifecycles
+// Warning about legacy string ref API usage
+// Warning about deprecated findDOMNode usage
+// Detecting unexpected side effects
+// Detecting legacy context API
+// Strict mode checks are run in development mode only; they do not impact the production build.
