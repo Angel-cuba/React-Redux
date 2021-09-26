3a12777a936c6ca61ae1faba9f0cb1dfd7a9e2b9
Angel <araozangel842@gmail.com>
2021-09-26 20:37:20 +0300

    Using dispatch and fetch from api file

--- client/react-redux/src/App.js
+++ client/react-redux/src/App.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import { useDispatch } from 'react-redux'
 import memories from './image/angel.png'
 
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts'
 
+import { getPosts } from './actions/post.actions'
+
 import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
 
+    useEffect(() => {
+      dispatch(getPosts())
+    }, [dispatch])
 
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="lg" className={classes.container}>
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
+        <Typography className={classes.typography} variant="h2" align="center">Memories</Typography>
         <img className={classes.image} src={memories} alt="" height="60"/>
       </AppBar>
       <Grow in>
           <Container>
               <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                   <Grid item xs={12} sm={7}>
-                    <h1>hahaha</h1>
+    
                     <Posts/>
                   </Grid>
                   <Grid item xs={12} sm={4}>
