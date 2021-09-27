4a0f81f32632f6af1398ffabd537f30d263a719e
Angel <araozangel842@gmail.com>
2021-09-27 21:47:14 +0300

    Update, likePost, deletePost functions in api folder OK

--- client/react-redux/src/api/api.js
+++ client/react-redux/src/api/api.js
@@ -5,3 +5,9 @@ const url = 'http://localhost:3002/api'
 export const fetchPost = () => axios.get(`${url}/read`)
 
 export const fetchCreate = (newPost) => axios.post(`${url}/create`,  newPost )
+
+export const updatePost = (  id, newPost ) => axios.put(`${url}/update/${id}`, newPost )
+
+export const deletePost = ( id ) => axios.delete(`${url}/delete/${id}`)
+
+export const likePost = ( id ) => axios.patch(`${url}/like/${id}`)
