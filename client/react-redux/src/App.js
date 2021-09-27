89e8515b687e0b7698f13945f8772f1925427c6b
Angel <araozangel842@gmail.com>
2021-09-27 21:44:05 +0300

    Pasando el ID del post desde el componente PADRE

--- client/react-redux/src/App.js
+++ client/react-redux/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 
 import { useDispatch } from 'react-redux'
@@ -12,29 +12,32 @@ import { getPosts } from './actions/post.actions'
 import useStyles from './styles'
 
 const App = () => {
+    const [ currentId, setCurrentId] = useState(null)
     const classes = useStyles()
     const dispatch = useDispatch()
-
+ 
     useEffect(() => {
       dispatch(getPosts())
-    }, [dispatch])
+    }, [currentId, dispatch])
 
   return (
     <Container maxWidth="lg" className={classes.container}>
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.typography} variant="h2" align="center">Memories</Typography>
         <img className={classes.image} src={memories} alt="" height="60"/>
+        
       </AppBar>
       <Grow in>
-          <Container>
+          <Container className={classes.content}>
               <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                <Grid item xs={12} sm={4}>
+                    <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                  </Grid>
                   <Grid item xs={12} sm={7}>
     
-                    <Posts/>
+                    <Posts setCurrentId={setCurrentId}/>
                   </Grid>
-                  <Grid item xs={12} sm={4}>
-                    <Form/>
-                  </Grid>
+                  
 
               </Grid>
           </Container>
