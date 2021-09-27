dde83c5ec869970dab69cb31db18e482acb73bcd
Angel <araozangel842@gmail.com>
2021-09-27 21:40:47 +0300

    New like function done

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 const router = Router();
 
-import { readPost, updatePost, createPost, deletePost } from '../controllers/controllers';
+import { readPost, updatePost, createPost, deletePost, likePost } from '../controllers/controllers';
 
 router.get('/read', readPost);
 router.post('/create', createPost);
-router.put('/update', updatePost);
+router.put('/update/:id', updatePost);
 router.delete('/delete/:id', deletePost);
+router.patch('/like/:id', likePost);
 
 
 export default router
