0dd748c4e0d43627b8ee50b20721e72da940e496
Angel <araozangel842@gmail.com>
2021-10-01 19:08:09 +0300

    Use ternary to, if user(DELETE, EDIT) and show some info

--- client/react-redux/src/components/Posts/Post/Post.js
+++ client/react-redux/src/components/Posts/Post/Post.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import useStyles from './styles'
 import { Card, CardActions, CardContent, CarMedia, Button, Typography, CardMedia, CircularProgress } from '@material-ui/core';
 
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 import { useDispatch } from 'react-redux';
 import { deletePost, likePost } from '../../../actions/post.actions'
@@ -17,21 +18,49 @@ import moment from 'moment';
 const Post = ({ post, setCurrentId }) => {
      // console.log(post);
      const classes = useStyles()
-     const { creator, title, createdAt, message, tags, likeCount,  _id } = post;
+     const { name, title, createdAt, message, tags, likes,  _id } = post;
      const dispatch = useDispatch();
 
-     const history = useHistory();
+     const user = JSON.parse(localStorage.getItem('profile'))
+     
+     console.log('likes----',post.likes.length)
+     // console.log(user.profile._id);
+     // console.log(user.profile.googleId);
+
+     const Likes = () => {
+            if((post.likes.length - 1) === 0){ <MoreHorizIcon/> }
+          if(post.likes.length > 0){
+               return post.likes.find((like) => like === (user.profile._id || user.profile.googleId)) 
+
+                    ? (
+                    <Fragment><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2  ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`
+                    // 
+                         }</Fragment>
+                    ) : (
+                         <Fragment>
+                            
+                         <ThumbUpOffAltIcon fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}
+                         </Fragment>
+                    )
+          }
+
+           return <Fragment><ThumbUpOffAltIcon fontSize="small"/>&nbsp;Like </Fragment> 
+      }
      
      return (
           !post ? <CircularProgress/> : (
                <Card className={classes.card} >
-           <CardMedia className={classes.media} title={title} component='img'/>
+           <CardMedia className={classes.media} title={title} image={post.selectedFile || 'https://res.cloudinary.com/dqaerysgb/image/upload/v1628766841/qpelx36hxluhbyfgcgdj.jpg'} component='img'/>
            <div className={classes.overlay}>
-                <Typography variant="h6">{creator}</Typography>
+                <Typography variant="h6">{name}</Typography>
                 <Typography className={classes.color} variant="h6">{title}</Typography>
 
                 <Typography style={{color: '#ce8713'}} variant="body2">{moment(createdAt).fromNow()}</Typography>
            </div>
+            {
+                         user ?
+                         (user.profile.googleId === post.creator || user.profile._id === post.creator)
+                              && (
                <div className={classes.overlay2}>
                     <Button 
                     styles={{ color: 'white' }} 
@@ -40,6 +69,10 @@ const Post = ({ post, setCurrentId }) => {
                          <MoreHorizIcon fontSize="default" />
                     </Button>
                </div>
+                  ) : (
+                                   null
+                              )
+                         }
                <div className={`${classes.details} ${classes.title}`}>
                      
                       <Typography className={classes.title1} variant="body2">{tags.map((tag) => `#${tag} `)}</Typography>
@@ -48,15 +81,26 @@ const Post = ({ post, setCurrentId }) => {
                       </CardContent>
 
                       <CardActions className={classes.cardActions}>
-                           <Button size="small" color="primary" onClick={ () => dispatch(likePost(_id))}>
-                                
-                                Like &nbsp;<ThumbUpAltIcon/>&nbsp;
-                                {likeCount}
+                           <Button size="small" color="primary" disabled={!user} onClick={ () => dispatch(likePost(_id))}>
+                               
+                                  {user ? < Likes /> : 'Like'}  
                            </Button>
-                           <Button className={classes.buttonDelete} fontSize="small" onClick={ () => dispatch(deletePost(_id))}>
+                               
+                           
+                         {
+                         user ?
+                         (user.profile.googleId === post.creator || user.profile._id === post.creator)
+                              && (
+                                   <Button className={classes.buttonDelete} fontSize="small" onClick={ () => dispatch(deletePost(_id))}>
                                 <DeleteIcon/>
-                                Delete
-                           </Button>
+                                     Delete
+                                </Button>
+                              ) : (
+                                   null
+                              )
+                         }
+                     
+                           
                       </CardActions>
                       
                </div>
