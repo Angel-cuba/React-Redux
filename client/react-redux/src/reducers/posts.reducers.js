ff48f6165361923ae8067bdbc519df9cda450d58
Angel <araozangel842@gmail.com>
2021-10-09 13:47:41 +0300

    Cambio de nombre(state) para poder recibir las variables desde el backend para la paginación

--- client/react-redux/src/reducers/posts.reducers.js
+++ client/react-redux/src/reducers/posts.reducers.js
@@ -1,25 +1,32 @@
 import {CREATE_POST, FETCH_ALL, FETCH_BY_SEARCH, UPDATE_POST, UPDATE_LIKE, DELETE_POST } from '../constants/actionsTypes';
 // eslint-disable-next-line import/no-anonymous-default-export
-export default ( posts = [], action) => {
+export default ( state = [], action) => {
      switch (action.type) {
 
           case FETCH_ALL:
-               return action.payload;
+               return {
+                    ...state,
+                    posts: action.payload.data, 
+                    currentPage: action.payload.currentPage,
+                    numberOfPages: action.payload.numberOfPages
+                    }
 
           case FETCH_BY_SEARCH:
-               return action.payload;
+               return { ...state, posts: action.payload };
 
           case CREATE_POST:
-               return [... posts, action.payload];
+               return [... state, action.payload];
 
           case UPDATE_POST:
+               return state.map((post) => post._id === action.payload._id ? action.payload : post);
+
           case UPDATE_LIKE:      
-               return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+               return state.map((post) => post._id === action.payload._id ? action.payload : post);
           
           case DELETE_POST:
-               return posts.filter((post) => post._id !== action.payload._id)
+               return state.filter((post) => post._id !== action.payload._id)
 
           default:
-               return posts;
+               return state;
      }
 }
