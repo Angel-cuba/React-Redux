6492799da43fc6d69f160af40c1bad37c97fef80
Angel <araozangel842@gmail.com>
2021-10-09 13:57:08 +0300

    Passing page props, numberOfPages object and working on pagination OK

--- client/react-redux/src/components/Pagination/Pagination.js
+++ client/react-redux/src/components/Pagination/Pagination.js
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Pagination, PaginationItem} from '@material-ui/lab'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getPosts } from '../../actions/post.actions'
 
 import useStyles from './styles';
 
 
-const Paginate = () => {
+const Paginate = ({ page }) => {
      const classes = useStyles()
+     const dispatch = useDispatch()
+	
+     const { numberOfPages } = useSelector((state) => state.posts)
+
+useEffect(() => {
+     if(page) dispatch(getPosts(page))
+},[page])
+
+
      return (
           <Pagination
-          className={classes.ul }
-          count={5}
-          page={1}
-          color="primary"
+          className={classes.ul}
+          count={numberOfPages}
+          page={Number(page) || 1}
+          color="standard"
           variant="outlined"
           renderItem={(item, index) => (
                
-                   <PaginationItem className={classes.paginationItem} {...item } component={Link} to={`/posts?page=${1}`} /> 
+                   <PaginationItem className={classes.paginationItem} {...item } component={Link} to={`/posts?page=${item.page}`} /> 
             
                
           )}
