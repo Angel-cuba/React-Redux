f2cd6bb0906bd0f45aee080ebbf1fe928d5c4af5
Angel <araozangel842@gmail.com>
2021-11-08 13:09:32 +0200

    Sigin redirect issue fixed

--- client/react-redux/src/actions/auth.js
+++ client/react-redux/src/actions/auth.js
@@ -1,26 +1,27 @@
-import * as api from '../api/api'
-import { AUTH } from '../constants/actionsTypes'
+import * as api from '../api/api';
+import { AUTH } from '../constants/actionsTypes';
 
-export const signin = ( formData, history) => async (dispatch) =>{
-     try {
-          const { data } = await api.signIn(formData)
+export const signin = (formData, history) => async (dispatch) => {
+	history.push('/auth');
+	try {
+		const { data } = await api.signIn(formData);
 
-          dispatch({ type: AUTH, data })
+		dispatch({ type: AUTH, data });
 
-         history.push('/') 
-     } catch (error) {
-          console.log(error);
-     }
-}
+		history.push('/posts');
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-export const signup = (formData, history) => async (dispatch) =>{
-     try {
-          const { data } = await api.signUp(formData)
+export const signup = (formData, history) => async (dispatch) => {
+	try {
+		const { data } = await api.signUp(formData);
 
-          dispatch({ type: AUTH, data })
-          
-          history.push('/')
-     } catch (error) {
-          console.log(error );
-     }
-}
+		dispatch({ type: AUTH, data });
+
+		history.push('/posts');
+	} catch (error) {
+		console.log(error);
+	}
+};
