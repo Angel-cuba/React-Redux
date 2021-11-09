1e16d9a3dfaa6dc2dde2c3fc735cc1fd3be6cfcb
Angel <araozangel842@gmail.com>
2021-11-09 20:13:16 +0200

    Fixed issue of to show 1, 2 or more likes

--- client/react-redux/src/components/Posts/Post/Post.js
+++ client/react-redux/src/components/Posts/Post/Post.js
@@ -8,7 +8,7 @@ import {
 	Typography,
 	CardMedia,
 	CircularProgress,
-	ButtonBase
+	ButtonBase,
 } from '@material-ui/core';
 
 import { useHistory } from 'react-router-dom';
@@ -24,28 +24,25 @@ import { deletePost, likePost } from '../../../actions/post.actions';
 
 import moment from 'moment';
 
-
 const Post = ({ post, setCurrentId }) => {
 	// console.log(post);
-	const classes = useStyles();
-	const { name, title, createdAt, message, tags,  _id, selectedFile } = post;
-	const dispatch = useDispatch();
-	const history = useHistory()
 
-	const [ likes, setLikes ] = useState(post.likes)
+	const classes = useStyles();
+	const { name, title, createdAt, message, tags, _id, selectedFile } = post;
+	const dispatch = useDispatch();
+	const history = useHistory();
+
+	const [likes, setLikes] = useState(post.likes);
 
 	const handleLikes = async () => {
-		dispatch(likePost(_id))
+		dispatch(likePost(_id));
 
-	 if (likes.find((like) => like === (user.profile._id || user.profile.googleId)) ){
-		 setLikes(likes.filter((like) => like !== (user.profile._id || user.profile.google)))
-	 }else{
-		 setLikes([ ...likes , user.profile._id || user.profile.googleId])
-	 }
-		
-		
-		
+		if (likes.find((like) => like === (user.profile._id || user.profile.googleId))) {
+			setLikes(likes.filter((like) => like !== (user.profile._id || user.profile.googleId)));
+		} else {
+			setLikes([...likes, user.profile._id || user.profile.googleId]);
 		}
+	};
 
 	const user = JSON.parse(localStorage.getItem('profile'));
 
@@ -54,17 +51,13 @@ const Post = ({ post, setCurrentId }) => {
 	// console.log(user.profile.googleId);
 
 	const Likes = () => {
-		if (likes.length - 1 === 0) {
-			 return <MoreHorizIcon />;
-			//return (<h1>No</h1>)
-		}
 		if (likes.length > 0) {
 			return likes.find((like) => like === (user.profile._id || user.profile.googleId)) ? (
 				<Fragment>
 					<ThumbUpAltIcon fontSize="small" />
 					&nbsp;
 					{
-						likes.length > 2
+						likes.length > 1
 							? `You and ${likes.length - 1} others`
 							: `${likes.length} like${likes.length > 1 ? 's' : ''}`
 						//
@@ -72,7 +65,7 @@ const Post = ({ post, setCurrentId }) => {
 				</Fragment>
 			) : (
 				<Fragment>
-					<ThumbUpOffAltIcon fontSize="small" />
+					<ThumbUpAltIcon fontSize="small" />
 					&nbsp;{likes.length} {likes.length === 1 ? 'Like' : 'Likes'}
 				</Fragment>
 			);
@@ -87,18 +80,13 @@ const Post = ({ post, setCurrentId }) => {
 	};
 
 	const openPost = () => {
-		history.push(`/read/${post._id}`)
-	}
+		history.push(`/read/${post._id}`);
+	};
 
 	return !post ? (
 		<CircularProgress />
 	) : (
 		<Card className={classes.card} raised elevation={6}>
-			
-
-			
-	
-
 			<CardMedia className={classes.media} title={title} image={selectedFile} component="div" />
 			<div className={classes.overlay}>
 				<Typography variant="h6">{name}</Typography>
@@ -110,9 +98,7 @@ const Post = ({ post, setCurrentId }) => {
 					{moment(createdAt).fromNow()}
 				</Typography>
 			</div>
-				
 
-			{/* <image></image> */}
 			{user
 				? (user.profile.googleId === post.creator || user.profile._id === post.creator) && (
 						<div className={classes.overlay2}>
@@ -122,63 +108,50 @@ const Post = ({ post, setCurrentId }) => {
 						</div>
 				  )
 				: null}
-				
-<ButtonBase
-				className={classes.buttonBase}
-				onClick={openPost}
-				>
-			<div className={classes.details}>
-				
-					
-				
-				<Typography className={classes.tags} variant="body2">
-					{tags.map((tag) => `#${tag.trim()} `)}
-				</Typography>
-				
-				<CardContent>
-					<Typography className={classes.title} component="p" variant="subtitle1">
-						{message}
+
+			<ButtonBase className={classes.buttonBase} onClick={openPost}>
+				<div className={classes.details}>
+					<Typography className={classes.tags} variant="body2">
+						{tags.map((tag) => `#${tag.trim()} `)}
 					</Typography>
-				</CardContent>
+
+					<CardContent>
+						<Typography className={classes.title} component="p" variant="subtitle1">
+							{message}
+						</Typography>
+					</CardContent>
 				</div>
 			</ButtonBase>
-				
-				<CardActions className={classes.cardActions}>
-					<Button
-						size="small"
-						color="primary"
-						disabled={!user}
-						// onClick={handleLikes}
-						onClick={handleLikes}
-					>
-						{user ? (
-							<Likes />
-						) : likes.length ? (
-							`This post has ${likes.length} likes`
-						) : (
-							<p style={{ color: 'white', textAlign: 'left' }}>
-								This post does not have any likes{' '}
-								<span role="img" arial-label="likes">
-									😢
-								</span>{' '}
-							</p>
-						)}
-					</Button>
 
-					{user
-						? (user.profile.googleId === post.creator || user.profile._id === post.creator) && (
-								<Button
-									className={classes.buttonDelete}
-									fontSize="small"
-									onClick={() => dispatch(deletePost(_id))}
-								>
-									<DeleteIcon />
-									Delete
-								</Button>
-						  )
-						: null}
-				</CardActions>
-			
+			<CardActions className={classes.cardActions}>
+				<Button size="small" color="primary" disabled={!user} onClick={handleLikes}>
+					{user ? (
+						<Likes />
+					) : likes.length ? (
+						<p style={{ color: 'white' }}>This post has {likes.length} likes</p>
+					) : (
+						<p style={{ textAlign: 'left', color: '#dedede' }}>
+							This post does not have any likes{' '}
+							<span role="img" arial-label="likes">
+								😢
+							</span>{' '}
+						</p>
+					)}
+				</Button>
+
+				{user
+					? (user.profile.googleId === post.creator || user.profile._id === post.creator) && (
+							<Button
+								className={classes.buttonDelete}
+								fontSize="small"
+								onClick={() => dispatch(deletePost(_id))}
+							>
+								<DeleteIcon />
+								Delete
+							</Button>
+					  )
+					: null}
+			</CardActions>
 		</Card>
 	);
 };
