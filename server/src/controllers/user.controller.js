82ff7f3886b02dde0bbe2f59d2ba9f1467a0882f
Angel <araozangel842@gmail.com>
2021-11-23 17:26:15 +0200

    Starting

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,74 +1,77 @@
-const ctrl = {}
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+const ctrl = {};
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-import User from '../models/user.model'
+import User from '../models/user.model';
 
 ctrl.sigInUser = async (req, res) => {
-     const { email, password } = req.body
+	const { email, password } = req.body;
 
-     try {
-          const existUser = await User.findOne({ email : email })
-          if(!existUser) return res.status(404).json({ message: 'User doesnt exist' })
+	try {
+		const existUser = await User.findOne({ email: email });
+		if (!existUser) return res.status(404).json({ message: 'User doesnt exist' });
 
-          const isPassword = await bcrypt.compare(password, existUser.password)
-          if(!isPassword) return res.status(403).json({ message: 'Wrong password' })
+		const isPassword = await bcrypt.compare(password, existUser.password);
+		if (!isPassword) return res.status(403).json({ message: 'Wrong password' });
 
-          const token = await jwt.sign(
-                    { 
-                    email: existUser.email, id: existUser._id
-                    }, 
-                     process.env.COOKIE_SECURE,
-                   //  'test',
-                     { 
-                          expiresIn: '1h'
-                     }
-                                                 )
-            res.status(200).json({ profile: existUser, token})
-               
-     } catch (error) {
-          res.status(500).json({ message: 'Something went wrong' })
-     }
-}
+		const token = await jwt.sign(
+			{
+				email: existUser.email,
+				id: existUser._id,
+			},
+			process.env.COOKIE_SECURE,
+			//  'test',
+			// {
+			// 	algorithm: ['RS384'],
+			// },
+			{
+				expiresIn: '1h',
+			}
+		);
+		res.status(200).json({ profile: existUser, token });
+	} catch (error) {
+		res.status(500).json({ message: 'Something went wrong' });
+	}
+};
 
 ctrl.signUpUser = async (req, res) => {
-     const { email, password, name, lastname, confirmpassword } = req.body
+	const { email, password, name, lastname, confirmpassword } = req.body;
 
-     try {
-          const existUser = await User.findOne({ email})
-          if(existUser) return res.status(404).json({ message: 'User already exist' })
+	try {
+		const existUser = await User.findOne({ email });
+		if (existUser) return res.status(404).json({ message: 'User already exist' });
 
-          if(password !== confirmpassword) return res.status(403).json({ message: "Passwords don't match" })
+		if (password !== confirmpassword)
+			return res.status(403).json({ message: "Passwords don't match" });
 
-          const encrypt = await bcrypt.genSalt(12)
-          const hashPassword = await bcrypt.hash(password, encrypt)
+		const encrypt = await bcrypt.genSalt(12);
+		const hashPassword = await bcrypt.hash(password, encrypt);
 
-          const newUser= new User({
-               name ,
-               lastname,
-               email,
-               password: hashPassword
-          })
+		const newUser = new User({
+			name,
+			lastname,
+			email,
+			password: hashPassword,
+		});
 
-          const result = await newUser.save()
-           const token = await jwt.sign(
-                    { 
-                    email: result.email, id: result._id
-                    }, 
-                    //  process.env.COOKIE_SECURE,
-                    // 'test',
-                    process.env.COOKIE_SECURE,
-                     { 
-                          expiresIn: '1h'
-                     }
-                                                 )
-            res.status(200).json({ 'profile': result, token})
-          
-     } catch (error) {
-          // res.status(500).json({ 'error': error, message: 'Something went wrong' })
-          console.log(error)
-          
-     }
-}
+		const result = await newUser.save();
+		const token = await jwt.sign(
+			{
+				email: result.email,
+				id: result._id,
+			},
+			//  process.env.COOKIE_SECURE,
+			// 'test',
+			process.env.COOKIE_SECURE,
+			{
+				expiresIn: '1h',
+			}
+		);
+		res.status(200).json({ profile: result, token });
+	} catch (error) {
+		// res.status(500).json({ 'error': error, message: 'Something went wrong' })
+		console.log(error);
+	}
+};
 
-module.exports = ctrl
+module.exports = ctrl;
--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,37 +1,35 @@
-import jwt, { decode } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 const auth = async (req, res, next) => {
+	// console.log(req.headers);
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		const isCustomAuthorization = token.length < 500;
+		console.log(token.length);
+		let decodedData;
 
-     //    console.log(req.headers)
-     try {
-          const token = req.headers.authorization.split(' ')[1];
-          const isCustomAuthorization = token.length < 500
+		if (token) {
+			decodedData = jwt.verify(token, process.env.COOKIE_SECURE);
+			//process.env.COOKIE_SECURE
+			// console.log('---------------------', decodedData);
+			// console.log(isCustomAuthorization);
+			// let finalDecodedData = jwt.verify(decodedData, process.env.COOKIE_SECURE, {
+			// 	algorithms: ['RS256'],
+			// });
+			console.log('---------------', decodedData);
 
-          // console.log('------',token);
-          //  console.log('-----------', isCustomAuthorization)
+			req.userId = decodedData.id;
+		} else {
+			decodedData = jwt.decode(token);
+			console.log(decodedData);
 
-          let decodedData
-                    // console.log(process.env.COOKIE_SECURE);
+			req.userId = decodedData.sub;
+		}
 
+		next();
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-               if(token || isCustomAuthorization){
-
-                    decodedData = jwt.verify(token, process.env.COOKIE_SECURE)
-                    //process.env.COOKIE_SECURE
-                    console.log('---------------------',decodedData)
-                    console.log(isCustomAuthorization)
-
-                    req.userId = decodedData.id
-               }else{
-                    decodedData = jwt.decode(token)
-
-                    req.userId = decodedData.sub
-               }
-
-               next();
-     } catch (error) {
-          console.log(error)
-     }
-}
-
-export default auth
+export default auth;
