03a0f8e6b1c405d94844bda6f5db3ecdb6e0d118
Angel <araozangel842@gmail.com>
2021-12-02 14:24:38 +0200

    Error messages of password or email to signin. Network error. All with SweetAlert2

--- client/react-redux/src/actions/auth.js
+++ client/react-redux/src/actions/auth.js
@@ -1,5 +1,6 @@
 import * as api from '../api/api';
 import { AUTH } from '../constants/actionsTypes';
+import Swal from 'sweetalert2';
 
 export const signin = (formData, history) => async (dispatch) => {
 	history.push('/auth');
@@ -10,7 +11,34 @@ export const signin = (formData, history) => async (dispatch) => {
 
 		history.push('/posts');
 	} catch (error) {
-		console.log(error);
+		if (error.message === 'Request failed with status code 404') {
+			// alert('los dos estan mal ...404');
+			Swal.fire({
+				titleText: 'Some error occurred while trying to sign in 👓',
+				text: 'Something is wrong or missing. Please try again.',
+				icon: 'error',
+				timer: 4000,
+				timerProgressBar: true,
+			});
+		}
+		if (error.message === 'Request failed with status code 403') {
+			Swal.fire({
+				titleText: 'Seems that maybe your password is incorrect 👓',
+				text: 'Check it again and try again.',
+				icon: 'error',
+				timer: 4000,
+				timerProgressBar: true,
+			});
+		}
+		if (error.message === 'Network Error') {
+			Swal.fire({
+				titleText: 'Sorry, we have some network issues 🛰️',
+				text: 'Please, wait a little and try again 🚀',
+				icon: 'info',
+				timer: 8000,
+				timerProgressBar: true,
+			});
+		}
 	}
 };
 
@@ -19,9 +47,25 @@ export const signup = (formData, history) => async (dispatch) => {
 		const { data } = await api.signUp(formData);
 
 		dispatch({ type: AUTH, data });
+		Swal.fire({
+			titleText: 'Has been nice to have you around',
+			text: 'Please, wait a little and try again 🚀',
+			icon: 'success',
+			timer: 2000,
+			timerProgressBar: true,
+		});
 
 		history.push('/posts');
 	} catch (error) {
 		console.log(error);
+		if (error.message === 'Request failed with status code 403') {
+			Swal.fire({
+				titleText: 'Seems like your password does not match 🔐.',
+				text: 'Check it again and try again 🥸.',
+				icon: 'error',
+				timer: 4000,
+				timerProgressBar: true,
+			});
+		}
 	}
 };
